96d4a85fdebd23bee5a897bcd4adfba41a2601ff
조영혜 <97926463+myjjjjjy@users.noreply.github.com>
2022-04-21 02:44:34 +0900

    Update MemberService.java
    회원가입

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -10,7 +10,11 @@ import java.util.Optional;
 // 서비스단은 네이밍 비지니스적으로!
 public class MemberService{
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     // 회원가입
     public Long join(Member member){
