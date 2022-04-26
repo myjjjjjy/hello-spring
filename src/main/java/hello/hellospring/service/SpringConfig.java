c523a9d0aa5688ca92369a06a9cda661e5dfd7b7
조영혜 <97926463+myjjjjjy@users.noreply.github.com>
2022-04-26 22:50:14 +0900

    오류 수정 완료! jbdc 시험하고 jpa로 수정했어야 됐는데 수정이 덜 된 부분이 있었음!

--- src/main/java/hello/hellospring/service/SpringConfig.java
+++ src/main/java/hello/hellospring/service/SpringConfig.java
@@ -1,22 +1,17 @@
 package hello.hellospring.service;
 
-import hello.hellospring.repository.JdbcMemberRepository;
-import hello.hellospring.repository.JdbcTemplateMemberRepository;
-import hello.hellospring.repository.MemberRepository;
-import hello.hellospring.repository.MemoryMemberRepository;
+import hello.hellospring.repository.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import javax.persistence.EntityManager;
-import javax.sql.DataSource;
 
 // 자바 코드로 스프링빈 주입하기
 @Configuration
 public class SpringConfig {
 
     private EntityManager em;
-
     @Autowired
     public SpringConfig(EntityManager em) {
         this.em = em;
@@ -31,7 +26,7 @@ public class SpringConfig {
     public MemberRepository memberRepository(){
         //return new JdbcMemberRepository(dataSource);
         //return new JdbcTemplateMemberRepository(dataSource);
-        return new JdbcMemberRepository(em);
+        return new JpaMemberRepository(em);
 
     }
 }
