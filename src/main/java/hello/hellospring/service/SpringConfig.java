c046acf2ac27e2a3ef68540b6a8d09782581113d
조영혜 <97926463+myjjjjjy@users.noreply.github.com>
2022-04-26 23:02:28 +0900

    jpa 방식으로 수정완료! interface 한문장으로 끝..?

--- src/main/java/hello/hellospring/service/SpringConfig.java
+++ src/main/java/hello/hellospring/service/SpringConfig.java
@@ -6,29 +6,36 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import javax.persistence.EntityManager;
+import javax.swing.*;
 
 // 자바 코드로 스프링빈 주입하기
 @Configuration
 public class SpringConfig {
 
-    private EntityManager em;
+    private final MemberRepository memberRepository;
+
     @Autowired
-    public SpringConfig(EntityManager em) {
-        this.em = em;
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
-    @Bean
-    public MemberService memberService(){
-        return new MemberService(memberRepository());
-    }
+//    private EntityManager em;
+//    @Autowired
+//    public SpringConfig(EntityManager em) {
+//        this.em = em;
+//    }
 
     @Bean
-    public MemberRepository memberRepository(){
-        //return new JdbcMemberRepository(dataSource);
-        //return new JdbcTemplateMemberRepository(dataSource);
-        return new JpaMemberRepository(em);
+    public MemberService memberService() {
 
+        return new MemberService(memberRepository);
     }
 }
 
-//
+//    @Bean
+//    public MemberRepository memberRepository(){
+        //return new JdbcMemberRepository(dataSource);
+        //return new JdbcTemplateMemberRepository(dataSource);
+        //return new JpaMemberRepository(em);
+ //   }
+//}
